9c8d1c7b204ba7673b15096d9bab922551b33ceb
Beingcms <102290041+Beingcms@users.noreply.github.com>
2026-05-11 21:34:47 +0100

    Refine Agentek positioning site and add social preview assets

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AgenTekV3 from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AgenTekV3 />
-  </React.StrictMode>,
-)
